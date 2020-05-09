f982c839625bc369140ff4c44d747e62fad9d4ed
kimji9663 <rlawlgp9663@naver.com>
2020-05-10 06:14:24 +0900

    Add page button style in 'blog'

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -37,38 +37,32 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
             </p>
           </header>
           <section dangerouslySetInnerHTML={{ __html: post.html }} />
-          <hr
+          {/* <hr
             style={{
               marginBottom: rhythm(1),
             }}
           />
           <footer>
             footer
-          </footer>
+          </footer> */}
         </article>
       </div>
 
-      <nav>
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
+      <nav className="other-post">
+        <ul>
           <li>
+            <span className="prev">Prev▲ </span>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
+                {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
+            <span className="next">Next▼ </span>
             {next && (
               <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
+                {next.frontmatter.title}
               </Link>
             )}
           </li>
